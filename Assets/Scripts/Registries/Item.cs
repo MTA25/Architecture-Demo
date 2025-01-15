24c96dd7b80b38d3cb75c574fc68e908c43a1e96
Talha <128969466+MTA25@users.noreply.github.com>
2025-01-16 00:30:51 +0500

    Setup classes

---
+++ Assets/Scripts/Registries/Item.cs
@@ -0,0 +1,24 @@
+public enum Item
+{
+    Pomegranate,
+    Pear,
+    Apple,
+    Umbrella,
+    Key,
+    Pent,
+    Pan,
+    Lock,
+    Pineapple,
+    Banana,
+    Thunder,
+    Shirt,
+    Socks,
+    Tyre,
+    Orange,
+    Pencil,
+    Lemon,
+    Shoes,
+    Book,
+    Truck,
+    None
+}
